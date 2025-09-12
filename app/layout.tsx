424050b4cfcd21f8a3aba4ce06c33942c6adff7f
solarxeng <murtaza@solarx.earth>
2025-09-12 00:40:57 -0700

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'SolarX Commission Calculator',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
